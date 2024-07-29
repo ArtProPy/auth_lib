e65dcefc7c394474f31e69af18e568fffa9013ba
Артем Проценко <aprocenko@it-fintech.com>
2024-07-29 16:20:57 +0300

    Добавлены __init__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='auth_lib',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     description='Авторизация пользователя',
     author='Art Pro',
---
+++ src/auth_lib/__init__.py
@@ -0,0 +1,2 @@
+from .serializers import BaseAuthValidator
+from .view import router
