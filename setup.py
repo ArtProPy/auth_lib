bdefebc9c0d9d04da3832c87e59f7438fec7a0f0
Артем Проценко <aprocenko@it-fintech.com>
2024-07-29 15:17:24 +0300

    Добавлена функция авторизации пользователя

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='auth_lib',
+    version='0.0.1',
+    packages=find_packages(),
+    description='Авторизация пользователя',
+    author='Art Pro',
+    author_email='ArtProPy@gmail.com',
+    url='https://github.com/ArtProPy/auth_lib',
+)
