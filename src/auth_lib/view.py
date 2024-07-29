2fd51b42dbce9e7a5fd7cba88677c90702bfbf92
Артем Проценко <aprocenko@it-fintech.com>
2024-07-29 17:00:44 +0300

    alpha 6

--- src/auth_lib/view.py
+++ src/auth_lib/view.py
@@ -2,6 +2,7 @@ import os
 
 import requests
 from fastapi.routing import APIRouter
+from starlette.responses import Response
 
 from src.auth_lib.serializers import BaseAuthValidator
 
@@ -9,9 +10,15 @@ router = APIRouter()
 
 
 @router.post('/login/')
-async def auth(data: BaseAuthValidator):
+async def auth(response: Response, data: BaseAuthValidator):
     """Авторизация пользователя."""
 
-    url = os.getenv('PATH_AUTH_API')
+    url = os.environ.get('PATH_AUTH_API')
+    if not url:
+        response.status_code = 400
+        return {'error': f'Не установлен `PATH_AUTH_API={url}`'}
 
-    return requests.post(url, data=dict(data)).json()
+    response_data = requests.post(url, data=dict(data)).json()
+    response.status_code = response_data.status_code
+
+    return response_data.json()
